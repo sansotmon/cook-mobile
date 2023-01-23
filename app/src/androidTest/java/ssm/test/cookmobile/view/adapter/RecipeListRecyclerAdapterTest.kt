c43df2572df1803a1c1dca1e77bbf76cc7705cb2
Santiago Soto <sansotmon@gmail.com>
2023-01-23 16:07:47 -0500

    added adapter test validations

---
+++ app/src/androidTest/java/ssm/test/cookmobile/view/adapter/RecipeListRecyclerAdapterTest.kt
@@ -0,0 +1,64 @@
+package ssm.test.cookmobile.view.adapter
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.PerformException
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import ssm.test.cookmobile.R
+import ssm.test.cookmobile.view.activity.RecipeListActivity
+
+
+class RecipeListRecyclerAdapterTest {
+
+    private val ITEM_POSITION = 5
+
+    @get:Rule
+    var activityScenarioRule: ActivityScenarioRule<RecipeListActivity> =
+        ActivityScenarioRule(RecipeListActivity::class.java)
+
+    @Test(expected = PerformException::class)
+    fun validateItemNotExist() {
+        onView(withId(R.id.recipeRecycler))
+            .perform(
+                RecyclerViewActions.scrollTo<RecipeListRecyclerAdapter.ViewHolder>(
+                    hasDescendant(withText("not in the list")))
+            )
+    }
+
+    @Test(expected = PerformException::class)
+    fun scrollToBelow() {
+        onView(withId(R.id.recipeRecycler))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecipeListRecyclerAdapter.ViewHolder>(
+                    ITEM_POSITION, click()
+                )
+            )
+    }
+
+    @Test
+    fun validateIsScrollAsRecycler() {
+        isRecyclable()
+    }
+
+    private fun isRecyclable(): Matcher<RecipeListRecyclerAdapter.ViewHolder?> {
+        return object : TypeSafeMatcher<RecipeListRecyclerAdapter.ViewHolder?>() {
+            override fun matchesSafely(item: RecipeListRecyclerAdapter.ViewHolder?): Boolean {
+                item?.let {
+                    return it.isRecyclable
+                }
+                return false
+            }
+
+            override fun describeTo(description: Description) {
+                description.appendText("is recyclable")
+            }
+        }
+    }
+}
